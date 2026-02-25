46b8fea9829ca263904ee36a4bcafea0a2ae14ee
Arghyajyoti Samui <108090661+Arghyajyoti007@users.noreply.github.com>
2026-02-25 22:43:00 +0530

    Create Basic While Loop.py

---
+++ RDP/Day7/Basic While Loop.py
@@ -0,0 +1,9 @@
+# Assignment 31: Basic While Loop
+# Write a Python program that uses a while loop to print numbers from 1 to 10.
+
+i = 1
+while True:
+    print(i)
+    i += 1
+    if i==11:
+        break
